865373f65dd87b4e46a6a709899cfc9d97a50dde
Yeasinoncode98 <devoncode98@gmail.com>
2025-08-01 02:01:20 +0600

    added

---
+++ js.strings_objects/object.js
@@ -0,0 +1,40 @@
+//primitive - basic,lowest+level
+//basic,primary datatype
+const age = 21;
+const school = "Rifle Square";
+const isPassed = true;
+// let isDeveloper;
+const subjects = ['bangla', 'English', 'physics','Math'];
+
+const bottle = ['White'];
+const bottleColor = 'White';
+const bottlePrice = 45;
+const bottleDrinks = 'water';
+
+//Object
+//non primitive
+                  //left er side gulake key / property bole 
+                  //right side er gulake value bole 
+                  //                   
+                  
+                  
+                  
+  const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'White',
+    isClean: false,
+
+}
+//non primitive
+//
+const subject = {
+    name : 'biology',
+    teacher: 'rasheda mam',
+    examDate: '30 Feb',
+    chapters : ['first','second','thrd'],
+    exams: {
+        name:'Final Exam',
+        marks : 100,
+    }
+}
