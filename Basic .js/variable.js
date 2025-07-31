e47f616fdfd198b7cdbe882880e87778cc54b91b
Yeasinoncode98 <devoncode98@gmail.com>
2025-07-31 22:27:03 +0600

    added

--- Basic .js/variable.js
+++ Basic .js/variable.js
@@ -31,3 +31,5 @@ console.log(age);
 
 // variable declare weight
 //variable is as same as c program
+
+//
---
+++ js.strings_objects/string.js
@@ -0,0 +1,34 @@
+const country = 'Bangladesh';
+
+const division = "NowaKhali"
+
+const district = `B-Baria`;
+
+// const thana = new String("Demra");
+
+// console.log( thana);
+
+const numbers = [53,45,7,8,9,34];
+console.log(numbers);
+
+numbers [1] = 56;
+console.log(numbers);
+
+// console.log(numbers.length);
+// console.log(numbers[2]);
+
+
+const capital = 'Dhaka';
+
+// console.log(capital.length);
+
+// console.log(capital[3]);
+
+//string is immutable string cant be changed 
+//Array is mutable array can be changed or delete at any time 
+
+console.log(capital);
+capital[1] = 'q';
+
+console.log(capital); // will not change the array index of [1] cause it is immutable
+
