c8c9ff7f38d5c3f26b6c195428bbe2832eb27741
Yeasinoncode98 <devoncode98@gmail.com>
2025-08-01 20:01:11 +0600

    added

---
+++ js.strings_objects/declare.js
@@ -0,0 +1,8 @@
+const pen = {brand : 'econo', price: 10, color:'Black'};
+const pencil = new Object();
+console.log(pencil);
+const rubber = Object.create({});
+console.log(rubber);
+
+//using class 
+//using new 
---
+++ js.strings_objects/loop.js
@@ -0,0 +1,26 @@
+const mobile = {
+    brand : 'samsung',
+    price : 25000,
+    color : 'black',
+    camera :'12mp',
+    isNew : true,
+}
+
+
+
+
+//for in : Object
+// for(const prop in mobile){
+    //     console.log(prop);
+    //     console.log(mobile[prop]);
+    // }
+    
+    
+
+//for of : Array
+const keys = Object.keys(mobile);
+console.log(keys);
+
+for(const key of keys){
+    console.log(key, ':' , mobile[key]);
+}
