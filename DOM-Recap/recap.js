a85f9a848de99a5c64040aeee66aa659cebe232f
Yeasinoncode98 <devoncode98@gmail.com>
2025-08-29 18:30:43 +0600

    dom -recap-added

---
+++ DOM-Recap/recap.js
@@ -0,0 +1,50 @@
+/*
+
+1.how to use js with html and css
+
+2.get element or elemets from dom
+
+2-1.getElementById -- specific kowke target korr jnno 
+
+2-2.getElementByClassName
+
+2-3.queryselector -- certain id er first element dibe
+
+2-4.queryselectorall -- certain id/class er shob element id shob dibe
+
+---> pet er vetor theke ber kore anar jnno querySelectorAll use kora lagbe
+
+
+
+3.Dynamically add element to the dom : document.createElement
+   3=5: Set value to the  innerHTML
+
+
+4.Get and set value from/to a dom ELement
+
+   4-5: use innerText ( h1,p,h5,div,span,strong)
+   4.2: use the value (input,textArea);
+
+
+5.Add evenListener (event handler) 
+   5-1: click,focus,blur,onchange,mouseenter,Keyup,keydown,
+   5-2: addEventListener , onclick="myHandler"
+   
+
+
+6.Use function to reduce duplicate code 
+
+
+
+7.Event bubble / Event delegate
+
+
+
+
+
+
+
+*/
+
+document.querySelectorAll(".users-container .disable h3");
+document.querySelectorAll(".my-users li");
---
+++ DOM-Recap/validation.js
@@ -0,0 +1,28 @@
+const number = 23;
+
+// string,number,boolean
+
+// if (typeof number === "number") {
+//   console.log("value is a number");
+// } else {
+//   console.log("Value is not a number");
+// }
+
+const numbers = [12, 78, 3, 90];
+const student = {
+  name: "Rongila Rafsan",
+  job: "khawadawa kora",
+};
+
+// console.log(Array.isArray(numbers));
+
+// console.log(Array.isArray(numbers));
+
+// TO chck is it array or not if array it will return yes else no
+
+// object
+
+// console.log(typeof student);
+
+const numberss = "kop";
+console.log(isNaN(numberss));
